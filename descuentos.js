a721c8b59c22e33ffc05caf29c7e0e5a5a41f0f8
mdiazc <spalduing12@gmail.com>
2022-03-14 18:09:57 -0500

    Added: Discounts by coupons

--- descuentos.js
+++ descuentos.js
@@ -21,3 +21,28 @@ function calcularPrecioConDescuento() {
 
   precioFinalParagraph.innerText = precioFinalMessage;
 }
+
+function calcularPrecioConDescuentoCupon() {
+  const descuentoSelectionado = {
+    mercadona: 20,
+    superinter: 40,
+    exito: 10,
+  };
+  const precioInput = document.getElementById('precioProducto');
+  const precio = Number(precioInput.value);
+
+  const descuentoInput = document.querySelector(
+    'input[name="discount"]:checked'
+  );
+  const descuento = descuentoInput.value;
+
+  const descuentoFinal = descuentoSelectionado[`${descuento}`];
+
+  const precioFinalParagraph = document.getElementById('precioFinal');
+
+  const precioConDescuento = getPrecioConDescuento(precio, descuentoFinal);
+
+  const precioFinalMessage = `El precio final es de: $${precioConDescuento}`;
+
+  precioFinalParagraph.innerText = precioFinalMessage;
+}
