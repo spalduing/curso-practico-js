2dc62777becb18d01e365a8a1a81a0529cc41537
spalduing <spalduing12@gmail.com>
2022-03-14 17:36:32 -0500

    Added: Static page. (#10)

--- descuentos.js
+++ descuentos.js
@@ -1,14 +1,23 @@
-function calcularPrecioConDescuento(precio, descuento) {
+function getPrecioConDescuento(precio, descuento) {
   const porcentajePrecioConDescuento = 100 - descuento;
 
   const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
-  //   console.log({
-  //     precio,
-  //     descuento,
-  //     porcentajePrecioConDescuento,
-  //     precioConDescuento,
-  //   });
-
   return precioConDescuento;
+}
+
+function calcularPrecioConDescuento() {
+  const precioInput = document.getElementById('precioProducto');
+  const precio = Number(precioInput.value);
+
+  const descuentoInput = document.getElementById('descuentoProducto');
+  const descuento = Number(descuentoInput.value);
+
+  const precioFinalParagraph = document.getElementById('precioFinal');
+
+  const precioConDescuento = getPrecioConDescuento(precio, descuento);
+
+  const precioFinalMessage = `El precio final es de: $${precioConDescuento}`;
+
+  precioFinalParagraph.innerText = precioFinalMessage;
 }
