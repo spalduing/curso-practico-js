d3017fc6bf881682870f8bd8b6bbc79bd58aa773
spalduing <spalduing12@gmail.com>
2022-03-21 15:56:20 -0500

    Added: Median code encapsulated into a function (#19)

--- mediana.js
+++ mediana.js
@@ -1,29 +1,32 @@
-let lista1 = [1, 2, 3, 4];
-let lista2 = [0, 1, 2, 3, 4, 5, 6];
+let list = [];
 
-// https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-lista1 = lista1.sort((a, b) => a - b);
-lista2 = lista2.sort((a, b) => a - b);
+function fillList() {
+  const listParagraph = document.getElementById('list');
+  const numberField = document.getElementById('numField');
 
-let isEven = (lista1.length | 1) !== lista1.length ? true : false;
-let isOdd = (lista2.length | 1) === lista2.length ? true : false;
+  const numValue = Number(numberField.value);
+  list.push(numValue);
 
-if (isEven) {
-  console.group('Par');
-  const midIndex1 = lista1.length / 2 - 1;
-  const midIndex2 = lista1.length / 2;
-  console.log('midIndex1: ', midIndex1);
-  console.log('midIndex2: ', midIndex2);
-  const mediana = (lista1[midIndex1] + lista1[midIndex2]) / 2;
-  console.log('mediana: ', mediana);
-  console.groupEnd();
+  listParagraph.innerText = `The list of numbers is: ${list}`;
 }
 
-if (isOdd) {
-  console.group('Impar');
-  const midIndex = (lista2.length / 2) | 0;
-  console.log('midIndex: ', midIndex);
-  const mediana = lista2[midIndex];
-  console.log('mediana: ', mediana);
-  console.groupEnd();
+function getMedian() {
+  const medianParagraph = document.getElementById('median');
+  let median = 0;
+
+  // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+  list = list.sort((a, b) => a - b);
+
+  let isEven = (list.length | 1) !== list.length ? true : false;
+
+  if (isEven) {
+    const midIndex1 = list.length / 2 - 1;
+    const midIndex2 = list.length / 2;
+    median = (list[midIndex1] + list[midIndex2]) / 2;
+  } else {
+    const midIndex = (list.length / 2) | 0;
+    median = list[midIndex];
+  }
+
+  medianParagraph.innerText = `The median is: ${median}`;
 }
